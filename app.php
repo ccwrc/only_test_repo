91da2957153f2439b8a14908050a6c9a43fd6887
ccwrc <j.muszelik@gmail.com>
2017-05-25 12:48:42 +0200

    app.php - work

--- app.php
+++ app.php
@@ -1,8 +1,34 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$urlTypicode = "http://jsonplaceholder.typicode.com/";
 
+$albumIds = [];
+$userIds = [];
+$result = [];
+
+
+foreach (json_decode(file_get_contents($urlTypicode . "photos")) as $photo) {
+    // var_dump($photo);
+   // if (strpos(get_object_vars($photo)["title"], "voluptate")) { //case sensitive !
+        if (stripos(get_object_vars($photo)["title"], "voluptate")) { //case insensitive
+        $albumIds[] = get_object_vars($photo)["albumId"];
+    }
+}
+// var_dump($albumIds);
+
+foreach (json_decode(file_get_contents($urlTypicode . "albums")) as $album) {
+    if (in_array(get_object_vars($album)["id"], array_unique($albumIds))) {
+        $userIds[] = get_object_vars($album)["userId"];
+    }
+}
+// var_dump(array_unique($albumIds));
+// var_dump($userIds);
+
+foreach (json_decode(file_get_contents($urlTypicode . "users")) as $user) {
+    if (in_array(get_object_vars($user)["id"], array_unique($userIds)) 
+            && preg_match("/^\d\d\d\d\d-\d\d\d\d$/", get_object_vars(get_object_vars($user)["address"])["zipcode"])) {
+        $result[] = ($user);
+    }
+}
+
+ var_dump($result);
