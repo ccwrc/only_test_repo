19365642d0eb5243f33f0510bb87985765f53619
ccwrc <j.muszelik@gmail.com>
2017-05-25 13:34:49 +0200

    remove comments, task done

--- app.php
+++ app.php
@@ -6,23 +6,18 @@ $albumIds = [];
 $userIds = [];
 $result = [];
 
-
 foreach (json_decode(file_get_contents($urlTypicode . "photos")) as $photo) {
-    // var_dump($photo);
-   // if (strpos(get_object_vars($photo)["title"], "voluptate")) { //case sensitive !
-        if (stripos(get_object_vars($photo)["title"], "voluptate")) { //case insensitive
+    // if (strpos(get_object_vars($photo)["title"], "voluptate")) { //case sensitive !
+    if (stripos(get_object_vars($photo)["title"], "voluptate")) { //case insensitive
         $albumIds[] = get_object_vars($photo)["albumId"];
     }
 }
-// var_dump($albumIds);
 
 foreach (json_decode(file_get_contents($urlTypicode . "albums")) as $album) {
     if (in_array(get_object_vars($album)["id"], array_unique($albumIds))) {
         $userIds[] = get_object_vars($album)["userId"];
     }
 }
-// var_dump(array_unique($albumIds));
-// var_dump($userIds);
 
 foreach (json_decode(file_get_contents($urlTypicode . "users")) as $user) {
     if (in_array(get_object_vars($user)["id"], array_unique($userIds)) 
@@ -31,4 +26,4 @@ foreach (json_decode(file_get_contents($urlTypicode . "users")) as $user) {
     }
 }
 
- var_dump($result);
+var_dump($result);
