d4787d7080fecb91cbddfd6fe380994c4f2411e2
ccwrc <j.muszelik@gmail.com>
2017-06-06 10:57:14 +0200

    appV4 - done

--- appV4.php
+++ appV4.php
@@ -7,26 +7,17 @@ $userIds = [];
 $result = [];
 
 foreach (json_decode(file_get_contents($urlTypicode . "photos?title_like=^.*voluptate.*$")) as $photo) {
-        $albumIds[] = $photo->albumId;
+    $albumIds[] = $photo->albumId;
 }
 
- var_dump($albumIds);
 foreach (json_decode(file_get_contents($urlTypicode . "albums")) as $album) {
     if (in_array($album->id, $albumIds)) {
         $userIds[] = $album->userId;
     }
 }
 
-//foreach (json_decode(file_get_contents($urlTypicode . "users")) as $user) {
-//    if (in_array($user->id, $userIds) 
-//            && preg_match("/^\d\d\d\d\d-\d\d\d\d$/", $user->address->zipcode)) {
-//        $result[] = $user;
-//    }
-//}
-
 foreach (json_decode(file_get_contents($urlTypicode . "users?address.zipcode_like=^\d\d\d\d\d-\d\d\d\d$")) as $user) {
     if (in_array($user->id, $userIds)) {
-         //   && preg_match("/^\d\d\d\d\d-\d\d\d\d$/", $user->address->zipcode)) {
         $result[] = $user;
     }
 }
