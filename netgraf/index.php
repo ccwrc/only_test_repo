8a5c01366ce420a807120f973feaed847e73d4f0
ccwrc <j.muszelik@gmail.com>
2017-09-29 16:28:53 +0200

    optimization

--- netgraf/index.php
+++ netgraf/index.php
@@ -12,7 +12,9 @@ function getArrayWithPaths() {
         fclose($handle);
     }
 
-    for ($i = 0; $i < count($csvArray); $i++) {
+    $counter = count($csvArray);
+    
+    for ($i = 0; $i < $counter; $i++) {
         $id = $csvArray[$i][0];
         $idParent = $csvArray[$i][1];
         $name = $csvArray[$i][2];
@@ -20,7 +22,7 @@ function getArrayWithPaths() {
         $endString = $name;
         $flag = $idParent;
         while (is_numeric($flag)) {
-            for ($j = 0; $j < count($csvArray); $j++) {
+            for ($j = 0; $j < $counter; $j++) {
                 if ($csvArray[$j][0] == $flag) {
                     $endString = $csvArray[$j][2] . " >> " . $endString;
                     $flag = $csvArray[$j][1];
