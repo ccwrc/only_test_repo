82bf48cd008392f44f6bc8f07fd266fca1cd97b0
ccwrc <j.muszelik@gmail.com>
2017-10-23 15:03:15 +0200

    add private function

--- testCi3/application/controllers/Blog.php
+++ testCi3/application/controllers/Blog.php
@@ -1,27 +1,33 @@
 <?php
 
-class Blog extends CI_Controller
-{
-    public function index() //index - load by default
-    { //http://localhost/only_test_repo/testCi3/index.php/blog
-      // or http://localhost/only_test_repo/testCi3/index.php/blog/index
+class Blog extends CI_Controller {
+
+    public function index() {
+        //index - load by default //http://localhost/only_test_repo/testCi3/index.php/blog
+        // or http://localhost/only_test_repo/testCi3/index.php/blog/index
         echo "Hello Blog controller! (function index)";
     }
-    
-    public function comments()
-    { // http://localhost/only_test_repo/testCi3/index.php/blog/comments
+
+    public function comments() { // http://localhost/only_test_repo/testCi3/index.php/blog/comments
         echo "public function comments in Blog controller";
     }
-    
-    public function _remap($method) // routing - god mode
-    { 
-        if($method === "testRemap")
-        {
+
+    public function _remap($method) { // routing - god mode
+        if ($method === "testRemap") {
             echo "_remap routing work (testRemap)";
-        }
-        else
-        {
+        } elseif ($method === "index") {
+            $this->index();
+        } elseif ($method === "comments") {
             $this->comments();
+        } elseif ($method === "private") {
+            $this->_prv();
+        } else {
+            show_404();
         }
     }
+
+    private function _prv() {
+        echo "private function";
+    }
+
 }
