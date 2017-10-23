3e8d2960f99186d976f114b86b335b8eb72d89e5
ccwrc <j.muszelik@gmail.com>
2017-10-23 13:31:19 +0200

    add comments function

--- testCi3/application/controllers/Blog.php
+++ testCi3/application/controllers/Blog.php
@@ -2,8 +2,14 @@
 
 class Blog extends CI_Controller
 {
-    public function index() 
+    public function index() //index - load by default
     { //http://localhost/only_test_repo/testCi3/index.php/blog
-        echo "Hello Blog controller!";
+      // or http://localhost/only_test_repo/testCi3/index.php/blog/index
+        echo "Hello Blog controller! (function index)";
+    }
+    
+    public function comments()
+    { // http://localhost/only_test_repo/testCi3/index.php/blog/comments
+        echo "public function comments in Blog controller";
     }
 }
