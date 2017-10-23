704aee3fc39bc70c2c216c597ae7feed6923929e
ccwrc <j.muszelik@gmail.com>
2017-10-23 13:13:19 +0200

    add blog controller

---
+++ testCi3/application/controllers/Blog.php
@@ -0,0 +1,9 @@
+<?php
+
+class Blog extends CI_Controller
+{
+    public function index() 
+    { //http://localhost/only_test_repo/testCi3/index.php/blog
+        echo "Hello Blog controller!";
+    }
+}
