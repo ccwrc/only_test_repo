df0e9c2bce85feff14e0febc0a03baab91e2e089
ccwrc <j.muszelik@gmail.com>
2017-10-23 13:37:35 +0200

    add function shoes

--- testCi3/application/controllers/Products.php
+++ testCi3/application/controllers/Products.php
@@ -2,6 +2,9 @@
 
 class Products extends CI_Controller
 {
-    //
+    public function shoes($sandals, $id)
+    { // http://localhost/only_test_repo/testCi3/index.php/products/shoes/paramSandals/paramId
+        echo $sandals . " + " . $id;
+    }
 }
 
