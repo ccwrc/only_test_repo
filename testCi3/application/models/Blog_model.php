4209bdc8aaf69cf48fc116b1b1e9548a9b46b880
ccwrc <j.muszelik@gmail.com>
2017-10-25 16:33:23 +0200

    add Blog_model

---
+++ testCi3/application/models/Blog_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Blog_model extends CI_Model {
+
+    var $id = "";
+    var $content = "";
+    var $author = "";
+
+    function __construct() {
+        parent::__construct();
+    }
+
+}
