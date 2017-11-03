871f6acd210744c013e67435f1cb0ea826d8ac90
ccwrc <j.muszelik@gmail.com>
2017-11-03 15:24:19 +0100

    datamapper simple test - OK

--- blog/application/controllers/Users.php
+++ blog/application/controllers/Users.php
@@ -3,6 +3,10 @@
 class Users extends CI_Controller {
     
     function index() {
-        echo "test users echo";
+        // echo "test users echo";
+        $testUser = new User();
+        $testUser->get_by_id(1)->to_array();
+        //var_dump($testUser);
+        echo $testUser->username;
     }
 }
