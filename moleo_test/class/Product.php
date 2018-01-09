f4c5328c98946862f1e296ee63ac5bf46c34268e
ccwrc <j.muszelik@gmail.com>
2018-01-09 19:00:06 +0100

    class Product - done

--- moleo_test/class/Product.php
+++ moleo_test/class/Product.php
@@ -5,15 +5,32 @@ class Product {
     public $name;
     public $price;
     public $quantity;
-    
+    private $amount = null;
+
     public function __construct($fileName) {
-        // TODO rzucić wyjątkiem jesli to niewlasciwy plik
+        if (!file_exists($fileName)) {
+            throw new ProductFileNotFound("plain exception message for product file...");
+        }
+
         $handle = fopen($fileName, "r");
         $jsonObject = json_decode(fgets($handle));
-        
+
         $this->id = $jsonObject->id;
         $this->name = $jsonObject->name;
         $this->price = $jsonObject->price;
         $this->quantity = $jsonObject->quantity;
     }
+    
+    public function getAmount() {
+        if ($this->amount === null) {
+            $this->amount = $this->price * $this->quantity;
+        }
+        return $this->amount;
+    }
+
+	public function __get($name) {
+        // TODO pole: wartosc pola... wtf?
+        return (isset($this->$name) ? $this->$name : null);
+    }
+
 }
--- moleo_test/index.php
+++ moleo_test/index.php
@@ -1,12 +1,24 @@
 <?php
 
+function classLoader($className) {
+    if(file_exists("class/" . $className . ".php")) {
+        require_once "class/" . $className . ".php";
+    }
+}
+
+spl_autoload_register("classLoader");
+
 echo "działa moleo <br/><br/>";
 
-$handle = fopen("product.json", "r");
+//$handle = fopen("product.json", "r");
+//
+//$jsonObject = json_decode(fgets($handle));
+//
+//var_dump($jsonObject->quantity);
 
-$jsonObject = json_decode(fgets($handle));
+$prod = new Product("product.json");
 
-var_dump($jsonObject->quantity);
+echo $prod->name;
 
 
 
